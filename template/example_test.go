4fdb3692042e228ad3150da1384f9ae392d398c8
John Stupka <hunter32292@users.noreply.github.com>
2022-01-07 10:35:39 -0600

    Update example_test.go
    Adding not implemented example as well

--- template/example_test.go
+++ template/example_test.go
@@ -1,14 +1,16 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+        // t.Fatal("not implemented")
+}
 
 func TestExample(t *testing.T) {
-	Run()
-	got := ""
-	expect := ""
-	if got != expect {
-		t.Errorf("got %q want %q", got, expect)
-	}
+        Run()
+        got := ""
+        expect := ""
+        if got != expect {
+                t.Errorf("got %q want %q", got, expect)
+        }
 }
