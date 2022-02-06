5b698adc201458aa5a18cbca4451bae420478713
John Stupka <hunter32292@users.noreply.github.com>
2022-02-06 15:37:09 -0600

    Update example_test.go

--- template/example_test.go
+++ template/example_test.go
@@ -14,3 +14,47 @@ func TestExample(t *testing.T) {
                 t.Errorf("got %q want %q", got, expect)
         }
 }
+
+
+
+
+import (
+	"testing"
+)
+
+func TestTimesThree(t *testing.T) {
+	tests := []struct {
+		name     string
+		send     int
+		channel  chan int
+		response int
+	}{
+		{
+			"Test 2 *3",
+			2,
+			make(chan int),
+			6,
+		},
+		{
+			"Test 3 * 3",
+			3,
+			make(chan int),
+			9,
+		},
+	}
+
+	for _, testcase := range tests {
+		t.Run(testcase.name, func(t *testing.T) {
+			// Setup
+			var result int
+			// Execute
+			go timesThree(testcase.send, testcase.channel)
+			// Check
+			result = <-testcase.channel
+			if result != testcase.response {
+				t.Errorf("Failure to calculate correct response, expected: %d -- response: %d", testcase.response, result)
+			}
+		})
+
+	}
+}
